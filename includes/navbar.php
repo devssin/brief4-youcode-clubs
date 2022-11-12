7ad7c31728904c31a8e155be23c85c9e2fe174c1
elhajuojy <souirimehdi311@gmail.com>
2022-11-12 18:02:39 +0100

    add some files and assets folder

---
+++ includes/navbar.php
@@ -0,0 +1,12 @@
+<nav class=" navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+  <p class="text-weight-bold"><a class="navbar-brand" href="#">youcode logo </a></p>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="form-inline my-2 my-lg-0">
+    <button class="btn btn-dark"><a href="../login/login.php">login</a> </button>
+  </div>
+  </div>
+</nav>
