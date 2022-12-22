ea3b2ae5969a51e367ae6f613351023063339a87
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2022-12-22 15:32:05 +0100

    remove  merage confilt

--- index.php
+++ index.php
@@ -129,12 +129,10 @@ $club_second = array_slice($clubs, 3, 3);
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
-<<<<<<< HEAD
+
     <?php include "./includes/script.inc.php" ?>
 
-=======
-    <?php include("./includes/script.inc.php"); ?>                                                            
->>>>>>> 13cd38a89816e1fc6f0107ca64d070efa36819aa
+
 </body>
 
 </html>
